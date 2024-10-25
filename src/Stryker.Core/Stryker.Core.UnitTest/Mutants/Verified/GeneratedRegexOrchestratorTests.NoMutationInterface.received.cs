aae5603ff1e213047ba29946d42c02f7f22403e8
Pentiva <git@pentiva.uk>
2024-10-25 14:22:25 +0100

    fix: Remove accidental VerifyTest received file

--- src/Stryker.Core/Stryker.Core.UnitTest/Mutants/Verified/GeneratedRegexOrchestratorTests.NoMutationInterface.received.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Text.RegularExpressions;
-namespace StrykerNet.UnitTest.Mutants.TestResources;
-public partial interface R {
-    [GeneratedRegex(@"a", RegexOptions.IgnoreCase, "en-US")]
-    private static partial Regex AbcGeneratedRegex();
-}
